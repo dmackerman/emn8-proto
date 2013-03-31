7cfebde0728c46c90fcc6560889b01c20dd5759b
Grgur Grisogono <grguru@gmail.com>
2013-04-01 00:54:18 +0200

    Most recent builds

--- build/ph-dev.js
+++ build/ph-dev.js
@@ -1,6 +1,13 @@
+/*! Pizza Hut Pilot App v0.0.1a 2013-04-01 00:26 */
 /*jslint browser:true */
-/*global $, requirejs, require */
+/*global $, Marionette */
 
 $(function () {
+    'use strict';
+    // Simply letting me know that everything is running ok
     $('body').append('<p>App is Go!!!</p>');
+
+    //Bootstrap the app
+    window.App = new Marionette.Application();
+
 });
