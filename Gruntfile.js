fca6e2800305a6d11006695e1e7f12dea391733f
Grgur Grisogono <grguru@gmail.com>
2013-03-31 22:58:36 +0200

    Banners on dev files + some comments on Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,11 @@ module.exports = function (grunt) {
     'use strict';
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        // Just concatenate files. Libs and project files are separate.
         concat: {
             options: {
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n',
                 separator: ';'
             },
             project: {
@@ -17,14 +20,17 @@ module.exports = function (grunt) {
                     'lib/jquery.js',
                     'lib/underscore.js',
                     'lib/json2.js',
-                    'lib/backbone.js'
+                    'lib/backbone.js',
+                    'lib/backbone.marionette.js'
                 ],
                 dest: 'build/libs.js'
             }
         },
+
+        // Minify project
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:ii") %> */\n'
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:ii") %> */\n'
             },
             project: {
                 files: {
@@ -50,9 +56,10 @@ module.exports = function (grunt) {
             }
         },
 
+        // Watch application js files and Gruntfile for changes
         watch: {
             scripts: {
-                files: ['app/**/*.js'],
+                files: ['app/**/*.js', 'Gruntfile.js'],
                 tasks: ['minimal'],
                 options: {
                     nospawn: true
@@ -61,13 +68,17 @@ module.exports = function (grunt) {
         }
     });
 
+    // Load task dependencies
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task(s).
+    // Default task - concat dev and watch for changes
     grunt.registerTask('default', ['concat', 'watch']);
-    grunt.registerTask('minimal', ['concat:project']);
-    grunt.registerTask('full', ['concat', 'uglify']);
 
+    // Task used for watch where only project is concatenated
+    grunt.registerTask('minimal', ['concat:project']);
+
+    // Task for a full build and minification
+    grunt.registerTask('full', ['concat', 'uglify']);
 };
