3337696460bc8dcbe0b6c4012fcfaf738a609347
Grgur Grisogono <grguru@gmail.com>
2013-03-31 23:49:06 +0200

    Marionette bootstrap

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
 /*jslint browser:true */
-/*global $, requirejs, require */
+/*global $, Marionette */
 
 $(function () {
+    'use strict';
+    // Simply letting me know that everything is running ok
     $('body').append('<p>App is Go!!!</p>');
+
+    //Bootstrap the app
+    window.App = new Marionette.Application();
+
 });
