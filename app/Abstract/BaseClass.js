0282a44036c38170de86811a24956c11fb1a2512
Grgur Grisogono <grguru@gmail.com>
2013-04-01 19:05:19 +0200

    callParent

--- app/Abstract/BaseClass.js
+++ app/Abstract/BaseClass.js
@@ -2,5 +2,9 @@ App.def('App.abstract.BaseClass', {
     init: function () {
         _.extend(this, Backbone.Events);
         this.trigger('init', this, arguments);
+    },
+
+    callParent: function (fn, args, scope) {
+        this.$super[fn].apply(scope || this, args);
     }
 });
