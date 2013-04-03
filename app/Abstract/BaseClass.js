a1fd5edfaa4fb75b926b43e7c9e933e501e472c7
Grgur Grisogono <grguru@gmail.com>
2013-04-03 12:49:35 +0200

    Config initialization

--- app/Abstract/BaseClass.js
+++ app/Abstract/BaseClass.js
@@ -1,10 +1,20 @@
 App.def('App.abstract.BaseClass', {
-    init: function () {
-        _.extend(this, Backbone.Events);
-        this.trigger('init', this, arguments);
+    init: function (config) {
+        'use strict';
+        var me = this;
+
+        _.extend(me, Backbone.Events);
+        me.trigger('init', me, arguments);
+        me.initConfig(config);
     },
 
     callParent: function (fn, args, scope) {
+        'use strict';
         this.$super[fn].apply(scope || this, args);
+    },
+
+    initConfig: function (config) {
+        'use strict';
+        _.extend(this, config);
     }
 });
